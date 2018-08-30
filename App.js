b5d1832bcf59a683ce4471a6fd8961e41efad31c
ytaniah9 <ytaniah9@gmail.com>
2018-08-30 15:46:58 +0700

    react_nativeku

---
+++ App.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import Judul from './Components/Judul'
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Judul></Judul>
+        <Text>Nama: Yohanna Tania Hartanto </Text>
+        <Text>Kelas: XI_RPL 4 </Text>
+        <Text>No.Absen: 35 </Text>
+        <Image style={{width: 500, height:400}}
+        source={require('./cici.jpg')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
---
+++ Components/Judul.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component{
+    render(){
+        return (
+            <Text style= {salon.Judul}
+                >BIODATA</Text>
+        )
+    }
+}
+
+const salon = {
+    Judul: {
+        color: '#f00',
+        fontSize: 50,
+        backgroundColor: '#60D7F8'
+    }
+}
+export default Judul;
